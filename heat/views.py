370161b9324d48c85c5eca4dc638a2b04cae2102
mwestphall <westphall@wisc.edu>
2016-01-02 01:28:25 +0000

    added Heroku requirements.txt

--- heat/views.py
+++ heat/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.conf import settings
 # Create your views here.
 from django.http import HttpResponse
-import numpy as np
 
 def home(request):
     return HttpResponse(settings.BASE_DIR)
